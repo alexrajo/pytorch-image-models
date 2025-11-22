088dde82912b9099e947870849dd37d3bde90392
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2025-11-22 22:58:17 +0100

    feat: add some more channels again

--- timm/models/resnet3d.py
+++ timm/models/resnet3d.py
@@ -851,7 +851,7 @@ default_cfgs = generate_default_cfgs({})
 @register_model
 def resnet3d_18(pretrained: bool = False, **kwargs) -> ResNet3D:
     """Constructs a ResNet3D-18 model."""
-    model_args = dict(block=BasicBlock3D, layers=(2, 2, 2, 2), channels=(8, 8, 16, 16))
+    model_args = dict(block=BasicBlock3D, layers=(2, 2, 2, 2), channels=(8, 16, 32, 64))
     return _create_resnet("resnet3d_18", pretrained, **dict(model_args, **kwargs))
 
 
